273fbddfc59527d1a5e58843a203c068f1419d8f
Jannik <1337741710@qq.com>
2025-05-18 20:25:10 +0800

    fix: 删除跨域重新在main.ts设置

--- apps/template-typeorm/src/common/config/cors.config.ts
+++
@@ -1,9 +0,0 @@
-import { INestApplication } from '@nestjs/common';
-
-export function setCors(app: INestApplication) {
-  app.enableCors({
-    origin: '*',
-    allowedHeaders: ['Authorization', 'content-type'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  });
-}
--- apps/template-typeorm/src/main.ts
+++ apps/template-typeorm/src/main.ts
@@ -2,21 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { AllExceptionFilter } from 'src/common/filters/all-exception.filter';
-import { setCors } from 'src/common/config/cors.config';
 import { ResponseInterceptor } from 'src/common/interceptors/response.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
-
   app.useGlobalFilters(new AllExceptionFilter());
-
   app.useGlobalInterceptors(new ResponseInterceptor());
-
-  setCors(app);
 
   await app.listen(port);
 }
+
 bootstrap();
