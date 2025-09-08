9bc4f6141544ec81fcd283200dd90988cd849fb7
poem7200 <shige720@qq.com>
2025-09-08 10:53:38 +0800

    feat: 添加template-typeorm模板的环境变量配置

--- apps/template-typeorm/src/app.controller.ts
+++ apps/template-typeorm/src/app.controller.ts
@@ -3,12 +3,19 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    // 需引入@nestjs/config，使用 ConfigService 获取环境变量
+    // private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
+    // 环境变量将自动按照顺序读取
+    // 1. .env.${process.env.NODE_ENV || 'development'}.local
+    // 2. .env.${process.env.NODE_ENV || 'development'}
+    // 3. .env.local
+    // 4. .env
     return this.appService.getHello();
   }
-
-  // 模板项目需要的是给一些示例，而非大量的内容
 }
