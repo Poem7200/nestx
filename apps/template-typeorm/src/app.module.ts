e3836c9ebf74523a9c4779f33062b79559b934f9
poem7200 <shige720@qq.com>
2025-09-21 22:46:25 +0800

    feat: 添加pino日志依赖

--- apps/template-typeorm/src/app.module.ts
+++ apps/template-typeorm/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -16,6 +17,7 @@ import { AppService } from './app.service';
       ],
       expandVariables: true,
     }),
+    LoggerModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
--- apps/template-typeorm/src/main.ts
+++ apps/template-typeorm/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AllExceptionsFilter } from './filters/all-exceptions.filter';
-import { ResponseTransformInterceptor } from './interceptors/response-transform.interceptor';
-import { AppModule } from './app.module';
+import { AllExceptionsFilter } from 'src/filters/all-exceptions.filter';
+import { ResponseTransformInterceptor } from 'src/interceptors/response-transform.interceptor';
+import { AppModule } from 'src/app.module';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const port = process.env.PORT ?? 3000;
 
   const app = await NestFactory.create(AppModule);
+
+  app.useLogger(app.get(Logger));
 
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new ResponseTransformInterceptor());
