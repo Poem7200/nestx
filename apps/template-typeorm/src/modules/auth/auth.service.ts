24e34abb8bc0b942750d81899c9c2918274a8018
poem7200 <shige720@qq.com>
2025-11-22 10:45:40 +0800

    feat: 添加refresh_token

--- apps/template-typeorm/src/modules/auth/auth.service.ts
+++ apps/template-typeorm/src/modules/auth/auth.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from './auth.strategy';
+import { ConfigService } from '@nestjs/config';
 
 export interface LoginResponse {
   access_token: string;
+  refresh_token: string;
 }
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
 
   /**
    * 生成 JWT token
@@ -16,9 +21,12 @@ export class AuthService {
    * @returns JWT token
    */
   generateToken(payload: JwtPayload): LoginResponse {
-    const access_token = this.jwtService.sign(payload);
+    // TODO: 这里的过期时间应该从配置中读取
+    const access_token = this.jwtService.sign(payload, { expiresIn: '1h' });
+    const refresh_token = this.jwtService.sign(payload, { expiresIn: '7d' });
     return {
       access_token,
+      refresh_token,
     };
   }
 }
